fecbe1c7413b583b50e78dcfced2d0fa496597f9
Alex Whaley <alex.whaley@mediatonic.co.uk>
2018-09-19 20:36:26 +0100

    Initial Push

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private bool _isInitialised = false;
+    private PlayerControls _playerControls;
+    private Rigidbody2D _playerRB;
+
+    [SerializeField] private float _playerSpeed = 1.0f;
+    [SerializeField] private SpriteRenderer _playerSprite;
+
+
+    private void Awake()
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    public void InitialisePlayer(PlayerConfig playerConfig)
+    {
+        _isInitialised = true;
+        _playerControls = playerConfig.PlayerControls;
+        _playerSprite.color = playerConfig.PlayerColor;
+
+    }
+
+    public void FixedUpdate()
+    {
+        if (!_isInitialised)
+        {
+            return;
+        }
+
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        var _playerVel = new Vector2(0.0f, 0.0f);
+        if (Input.GetKeyDown(_playerControls.UpKey))
+        {
+            _playerVel.y += _playerSpeed;
+        }
+        if (Input.GetKeyDown(_playerControls.DownKey))
+        {
+            _playerVel.y -= _playerSpeed;
+        }
+        if (Input.GetKeyDown(_playerControls.RightKey))
+        {
+            _playerVel.x += _playerSpeed;
+        }
+        if (Input.GetKeyDown(_playerControls.LeftKey))
+        {
+            _playerVel.x -= _playerSpeed;
+        }
+
+        _playerRB.velocity = _playerVel;
+    }
+}
+
+public struct PlayerConfig
+{
+    public PlayerControls PlayerControls;
+    public Color PlayerColor;
+}
+
+public struct PlayerControls
+{
+    public string UpKey;
+    public string DownKey;
+    public string LeftKey;
+    public string RightKey;
+}
---
+++ Assets/TwitchChat.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TwitchChat : MonoBehaviour
+{
+    private TcpClient _twitchClient;
+    private StreamReader _reader;
+    private StreamWriter _writer;
+
+    [SerializeField]
+    private string _username;
+    [SerializeField]
+    private string _password;
+    [SerializeField]
+    private string _channelName;
+
+    [SerializeField]
+    private Text _twithChatBox;
+
+    private const string TwitchHostname = "irc.chat.twitch.tv";
+
+
+    void Start ()
+    {
+        Connect();
+    }
+
+    private void Update()
+    {
+        if (!_twitchClient.Connected)
+        {
+            Debug.Log("Not connected to twitch chat attempting to reconnect.");
+            Connect();
+        }
+
+        ReadChat();
+    }
+    
+    private void Connect()
+    {
+        _twitchClient = new TcpClient(TwitchHostname, 6667);
+        _reader = new StreamReader(_twitchClient.GetStream());
+        _writer = new StreamWriter(_twitchClient.GetStream());
+
+        _writer.WriteLine("PASS " + _password);
+        _writer.WriteLine("NICK " + _username);
+        _writer.WriteLine("USER " + _username + " 8 * :" + _username);
+        _writer.WriteLine("JOIN #" + _channelName);
+        _writer.Flush();
+    }
+
+    private void ReadChat()
+    {
+        if (_twitchClient.Available > 0)
+        {
+            var message = _reader.ReadLine();
+
+            if (message.Contains("PRIVMSG"))
+            {
+                //Get the users name by splitting it from the string
+                var splitPoint = message.IndexOf("!", 1);
+                var chatName = message.Substring(0, splitPoint);
+                chatName = chatName.Substring(1);
+
+                //Get users message by splitting it from the string
+                splitPoint = message.IndexOf(":", 1);
+                message = message.Substring(splitPoint + 1);
+
+                _twithChatBox.text = _twithChatBox.text + "\n" + string.Format("{0}: {1}", chatName, message);
+            }
+        }
+    }
+}
